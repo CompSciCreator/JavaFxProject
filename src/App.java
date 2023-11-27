9fd9277c9d798d208ac029cb79b4cdf69d650bff
Andrew Paolella <147566291+CompSciCreator@users.noreply.github.com>
2023-11-27 11:40:45 -0500

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,44 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent; 
+
+    public class App extends Application {
+        double x, y = 0;
+
+        public void start(Stage stage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("fxml/Bargraph.fxml"));
+            Scene sc = new Scene(root, 1000, 700);
+   
+            
+
+            
+            
+            
+            root.setOnMousePressed(evt -> {
+                x = evt.getSceneX();
+                y = evt.getSceneY();
+            });
+            root.setOnMouseDragged(evt -> {
+                stage.setX(evt.getScreenX()- x);
+                stage.setY(evt.getScreenY()- y);
+            });
+
+            stage.setScene(sc);
+            stage.show();
+        }
+
+            
+
+
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+
+
+}
